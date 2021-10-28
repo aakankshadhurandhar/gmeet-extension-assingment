ccf3cdc40df62040bcb5456a2325e3733e0b4ad5
aakankshadhurandhar <aakankshadhurandhar34@gmail.com>
2021-10-28 12:18:43 +0530

    added manifest v2

--- webpack.config.js
+++ webpack.config.js
@@ -183,6 +183,18 @@ var options = {
       cache: false,
     }),
     new HtmlWebpackPlugin({
+      template: path.join(
+        __dirname,
+        'src',
+        'pages',
+        'Background',
+        'index.html'
+      ),
+      filename: 'background.html',
+      chunks: ['background'],
+      cache: false,
+    }),
+    new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.html'),
       filename: 'devtools.html',
       chunks: ['devtools'],
